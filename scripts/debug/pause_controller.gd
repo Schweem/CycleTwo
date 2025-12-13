16399b748798f7f989d6ffbae4789ac1c1ec41b6
Schweem <seamusdjackson@gmail.com>
2025-12-13 12:49:50 -0500

    Project structure cleanup. F1 debug menu toggle

--- scripts/debug/pause_controller.gd
+++ scripts/debug/pause_controller.gd
@@ -2,12 +2,23 @@
 
 extends Node
 
+var children : Array = []
+
 func _ready() -> void:
-    process_mode = Node.PROCESS_MODE_ALWAYS
+	# Store children (labels) in array
+	children = get_children()
+	
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventKey:
-        if event.keycode == KEY_F2 and event.pressed and not event.echo:
-            get_tree().paused = not get_tree().paused
-            get_viewport().set_input_as_handled()
-            print("pause")
+	if event is InputEventKey:
+		# Toggle Debug Menu
+		if event.keycode == KEY_F1 and event.pressed and not event.echo:
+			for child in children:
+				child.visible = !child.visible
+		
+		# Handle game pause
+		if event.keycode == KEY_F2 and event.pressed and not event.echo:
+			get_tree().paused = not get_tree().paused
+			get_viewport().set_input_as_handled()
+			print("pause")
