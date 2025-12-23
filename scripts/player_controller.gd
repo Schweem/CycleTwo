5f979818a8110232459f97cb99a7a6122a5fac9d
schweem <seamusdjackson@gmail.com>
2025-12-23 16:48:19 -0500

    basic walljump to player

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -8,9 +8,13 @@ const JUMP_SPEED : float = -350.0
 
 # regular vars
 var speed_mult : float = 1.0
+
 # variable jump height stuff
 var jump_res : float = 3.0
 var coyote_time : float = 1.0
+
+var walljump : bool = false
+var jumpCancelled : bool = false
 
 # gravity from engine 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -29,6 +33,14 @@ func _physics_process(delta: float) -> void:
 			coyote_time -= 0.1
 		velocity.y += gravity * delta
 	else:
+		# reset all the jump stuff
+		# state machines would be sick right 
+		if walljump == true:
+			walljump = false
+
+		if jumpCancelled == true:
+			jumpCancelled = false
+
 		if coyote_time < 1.0:
 			coyote_time += 0.1
 
@@ -45,11 +57,17 @@ func handle_inputs() -> void:
 		if coyote_time > 0:
 			coyote_time = 0
 			velocity.y = JUMP_SPEED
+	
+		if is_on_wall_only() and (walljump == false):
+			velocity.y = JUMP_SPEED * 1.2
+			velocity.x = -2
+			walljump = true
 
 	# variable jump height
 	if !is_on_floor():
-		if Input.is_action_just_released("jump"):
+		if Input.is_action_just_released("jump") and (jumpCancelled == false):
 			velocity.y = velocity.y / jump_res
+			jumpCancelled = true
 
 	if Input.is_action_just_pressed("draw"):
 		sword.drawSword()
